4942e0b2cc2efbbab72a41741dceb0cbee00ea32
Aaron Hall <c.aaron.hall@gmail.com>
2020-10-04 19:13:12 -0500

    Have a very rough version working TODO:implement showing current counts correctly implement timers for showing bad cards and moving cards between piles implement bank button and functionality network between multiple players

--- speedguts.py
+++ speedguts.py
@@ -1,7 +1,8 @@
 import os
-
+import pygame
 # Import other modules from pygame_cards if needed.
-from pygame_cards import game_app, controller, enums, card_holder, deck, card
+from pygame_cards import game_app, controller, enums, card_holder, deck, card, gui
+from random import shuffle
 
 class position:
     def __init__(self,x=0,y=0):
@@ -14,19 +15,46 @@ class slot:
         self.card = card
 
 
+class State:
+    """ Enums for state machine """
+    INIT = 0
+    START = 3
+    PLAY = 4
+    TIMER_ON = 5
+    END = 6
+
 def draw_empty_card_pocket(holder, screen):
     """ Renders empty card pocket at the position of CardHolder object
     :param holder: CardsHolder object
     :param screen: Screen object to render onto
     """
     if len(holder.cards) == 0:
-        rect = (holder.pos.x, holder.pos.y,
+        rect = (holder.pos[0], holder.pos[1],
                 card_holder.CardsHolder.card_json["size"][0],
                 card_holder.CardsHolder.card_json["size"][1])
         pygame.draw.rect(screen, (77, 77, 77), rect, 2)
 
 
-#class Foundation(card_holder.CardsHolder):
+class Discards(card_holder.CardsHolder):
+
+    def render(self, screen):
+        draw_empty_card_pocket(self, screen)
+
+
+class Upcards(card_holder.CardsHolder):
+
+    def move_all_cards(self, other, back_side_up=True):
+        """ Moves all cards to other cards holder.
+        :param other: instance of CardsHolder where cards will be moved.
+        :param back_side_up: True if cards should be flipped to back side up, False otherwise.
+        """
+        if isinstance(other, CardsHolder):
+            while len(self.cards) != 0:
+                card_ = self.pop_top_card()
+                if card_ is not None:
+                    if card_.back_up != back_side_up:
+                        card_.flip()
+                    other.add_card(card_)
 
 
 class MyGameController(controller.Controller):
@@ -54,43 +82,40 @@ class MyGameController(controller.Controller):
 
         deck_pos = self.settings_json["deck"]["position"]
         deck_offset = self.settings_json["deck"]["offset"]
-        cards_pos = self.settings_json["cards"]["position"]
         self.nCards = self.settings_json["gui"]["num_cards"]
-        self.custom_dict["deck"] = deck.Deck(type_=enums.DeckType.short,
-                                             pos=deck_pos, offset=deck_offset)
-        #tmp = Card(hearts,two,(deck_pos[0],deck_pos[1]))
-        #self.tmp_card = tmp
-        pos = (cards_pos[0],cards_pos[1])
+        self.deck = deck.Deck(type_=enums.DeckType.full, pos=deck_pos, offset=deck_offset)
         card_size = self.settings_json["card"]["size"]
-        self.slots = [0] * self.nCards
-        self.current_card_index = 0
-        pos = (cards_pos[0] + (0 * (card_size[0] + 20)), cards_pos[1])
-        self.slots[0] = card_holder.CardsHolder(pos, (0, 0))
-        self.add_rendered_object((self.custom_dict["deck"], self.slots[0]))
-        for i in range(1, self.nCards):
-            offset = (i * (card_size[0]+20), 0)
-            pos = (cards_pos[0] + (i * (card_size[0]+20)),cards_pos[1])
-            self.slots[i] = card_holder.CardsHolder(pos, (0,0))
-#            self.add_rendered_object((self.custom_dict["deck"], self.slots[i]))
-            #self.add_rendered_object((self.custom_dict["deck"], self.slots[i]))
-            #self.add_rendered_object((self.tmp_card, self.slots[i]))
-            #self.slots[i] = slot(deck_pos[0] + (i*card_size[0]),deck_pos[1],None)
+        upcards_pos = self.settings_json["upcards"]["position"]
+        upcards_offset = self.settings_json["upcards"]["offset"]
+        self.upcards = card_holder.CardsHolder(pos=upcards_pos, offset=upcards_offset)
 
-        for i in range(1,self.nCards,2):
-            self.add_rendered_object((self.slots[i], self.slots[i+1]))
+        discards_pos = self.settings_json["discards"]["position"]
+        discards_offset = self.settings_json["discards"]["offset"]
+        self.discards = Discards(pos=discards_pos, offset=discards_offset)
+
+        self.tmpdeck = card_holder.CardsHolder(pos=(0,0))
+
+        self.current_card_index = 0
+        self.bank_index = 0
+        self.add_rendered_object((self.deck, None))
+        self.add_rendered_object((self.upcards, self.discards))
 
         # All game objects should be added to self objects list
         #  with add_object method in order to be rendered.
-        #self.add_rendered_object((self.custom_dict["deck"], self.custom_dict["stack"]))
-        #self.add_rendered_object((self.custom_dict["deck"],self.slots))
 
         # Create Restart button
         self.gui_interface.show_button(self.settings_json["gui"]["restart_button"],
                                        self.restart_game, "Restart")
         self.gui_interface.show_button(self.settings_json["gui"]["higher_button"],
                                        self.higher_clicked, "Higher")
+        self.gui_interface.show_button(self.settings_json["gui"]["bank_button"],
+                                       self.bank_clicked, "Bank")
         self.gui_interface.show_button(self.settings_json["gui"]["lower_button"],
                                        self.lower_clicked, "Lower")
+
+        #self.gui_interface.show_label(self.settings_json["gui"]["count_label"], "Count:0")
+        self.show_count()
+        self.state = State.INIT
 
 
     def start_game(self):
@@ -101,16 +126,15 @@ class MyGameController(controller.Controller):
 
         # Shuffle cards in the deck
         self.current_card_index = 1
-        self.custom_dict["deck"].shuffle()
-        for i in range(0, self.nCards):
-            pass
-            card_ = self.custom_dict["deck"].pop_top_card()
-            self.slots[i].add_card(card_)
+        self.bank_index = 0
+        self.deck.shuffle()
+        card_ = self.deck.pop_top_card()
+        card_.flip()
+        self.upcards.add_card(card_)
 
-        _card = self.slots[0].pop_top_card()
-        _card.flip()
-        self.slots[0].add_card(_card)
-
+    def show_count(self):
+        tmp = "Count:%i" % (self.current_card_index)
+        self.gui_interface.show_label(self.settings_json["gui"]["count_label"], tmp, timeout=0)
 
     def process_mouse_event(self, pos, down, double_click):
         """ Put code that handles mouse events here.
@@ -121,9 +145,9 @@ class MyGameController(controller.Controller):
             :param down: boolean, True for mouse down event, False for mouse up event
             :param double_click: boolean, True if it's a double click event
         """
-#        if down and self.custom_dict["deck"].is_clicked(pos):
-        if down and self.custom_dict["deck"].is_clicked(pos):
-            card_ = self.custom_dict["deck"].pop_top_card()
+#        if down and self.deck.is_clicked(pos):
+        if down and self.deck.is_clicked(pos):
+            card_ = self.deck.pop_top_card()
             if isinstance(card_, card.Card):
                 card_.flip()
                 #self.custom_dict["stack"].add_card(card_)
@@ -133,7 +157,8 @@ class MyGameController(controller.Controller):
             start_game() method can be called here to avoid code duplication. For example,
             This method can be used after game over or as a handler of "Restart" button.
         """
-        #self.custom_dict["stack"].move_all_cards(self.custom_dict["deck"])
+        self.upcards.move_all_cards(self.deck)
+        self.discards.move_all_cards(self.deck)
         self.start_game()
 
     def execute_game(self):
@@ -154,25 +179,69 @@ class MyGameController(controller.Controller):
         """ Called when user closes the app.
             Add destruction of all objects, storing of game progress to a file etc. to this method.
         """
-        del self.custom_dict["deck"]
-        #del self.custom_dict["stack"]
+        del self.deck
+        del self.upcards
+        del self.discards
+        del self.tmpdeck
+
+    def disable_buttons(self):
+        pass
+
+    def enable_buttons(self):
+        pass
+
+    def start_timer(self, ms=500):
+        #start timer, not event driver, must poll
+        pass
+
+    def show_bad_card_then_reset(self):
+        # start a timer to show the bad card, timeout, then move
+        # all cards from upcards back to deck
+        # need state machine that is updated in execute_game, calls en/disable buttons
+        self.move_all_but_bottom_card(self.upcards,self.deck)
+        pass
+    
+    def move_all_but_bottom_card(self,src,dst,doShuffle=True):
+        while (len(src.cards) > 1):
+            _card = src.pop_top_card()
+            _card.flip()
+            self.tmpdeck.add_card(_card)
+        if doShuffle == True:
+            shuffle(self.tmpdeck.cards)
+        self.tmpdeck.move_all_cards(dst)
 
     def higher_clicked(self):
         pass
         if self.current_card_index < self.nCards:
-            _card = self.slots[self.current_card_index].pop_top_card()
-            _card.flip()
-            self.slots[self.current_card_index].add_card(_card)
-            self.current_card_index += 1
+            new_card = self.deck.pop_top_card()
+            new_card.flip()
+            up_card = self.upcards.pop_top_card()
+            self.upcards.add_card(up_card)  # put that card back
+            self.upcards.add_card(new_card)  # put on new card
+            if new_card.rank > up_card.rank:
+                self.current_card_index += 1
+            else:
+                self.show_bad_card_then_reset()
+            self.show_count()
 
     def lower_clicked(self):
         pass
         if self.current_card_index < self.nCards:
-            _card = self.slots[self.current_card_index].pop_top_card()
-            _card.flip()
-            self.slots[self.current_card_index].add_card(_card)
-            self.current_card_index += 1
+            new_card = self.deck.pop_top_card()
+            new_card.flip()
+            up_card = self.upcards.pop_top_card()
+            self.upcards.add_card(up_card)  # put that card back
+            self.upcards.add_card(new_card)  # put on new card
+            if new_card.rank < up_card.rank:
+                self.current_card_index += 1
+            else:
+                self.show_bad_card_then_reset()
+            self.show_count()
 
+    def bank_clicked(self):
+        self.bank_index = self.current_card_index
+        # set a timer to disable buttons
+        self.show_bad_card_then_reset()
 
 def main():
     """ Entry point of the application. """
